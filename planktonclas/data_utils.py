60632387fe95978428f7d243010746f92b046a55
LeoDuda <leo.dud97@gmail.com>
2025-04-29 09:49:27 +0200

    changed coarsedropout due to deprecated parameters

--- planktonclas/data_utils.py
+++ planktonclas/data_utils.py
@@ -464,13 +464,10 @@ def augment(im, params=None):
         scale = np.random.uniform(scale_low, scale_high)
         transform_list.append(
             A.CoarseDropout(
-                min_holes=8,
-                max_holes=8,
-                min_height=int(scale * ly),
-                max_height=int(scale * ly),
-                min_width=int(scale * lx),
-                max_width=int(scale * lx),
-                fill_value=0,  # Default is black, change if you want a different fill
+                num_holes_range=(8, 8), 
+                hole_height_range=(int(scale * ly), int(scale * ly)), 
+                hole_width_range=(int(scale * lx), int(scale * lx)),  
+                fill=0, 
                 p=params["cutout"],
             )
         )
